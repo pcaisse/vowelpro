14f1f17cb014c2ea56b30443948da90170f2b43a
Peter Caisse <pcaisse@gmail.com>
2014-03-15 15:25:58 -0400

    Showing spectrogram

--- sp.py
+++ sp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import wave
 
 def get_vowel_range(wav):
@@ -15,7 +15,7 @@ def get_vowel_range(wav):
     
     # Extract raw audio from WAV file
     signal = spf.readframes(-1)
-    signal = numpy.fromstring(signal, 'Int16')
+    signal = np.fromstring(signal, 'Int16')
     f = spf.getframerate()
 
     max_val = max(signal)
@@ -26,7 +26,7 @@ def get_vowel_range(wav):
     signal_pos = [signal[x] if signal[x] > 0 else 1 for x in xrange(0, len(signal))]
 
     # Get averages within buckets
-    means = [int(numpy.mean(signal_pos[i:i+bucket_size])) for i in xrange(0, len(signal_pos), bucket_size)]
+    means = [int(np.mean(signal_pos[i:i+bucket_size])) for i in xrange(0, len(signal_pos), bucket_size)]
 
     # Get spikes
     spike_indexes = [i*bucket_size for i in xrange(0, len(means)) if i > 0 and means[i-1]*spike_factor < means[i]][:2]
@@ -41,12 +41,18 @@ def get_vowel_range(wav):
 
     print vowel_range
 
-    # Plot graph
+    # Plot waveform
+    plt.subplot(211)
     plt.plot(signal)
+
     # Plot vowel
     for index in vowel_range:
         plt.plot([index, index], [max_val*-1, max_val], 'k-', lw=3, color='yellow', linestyle='dashed')
     plt.plot([vowel_index, vowel_index], [max_val*-1, max_val], 'k-', lw=3, color='red', linestyle='solid')
+
+    # Plot spectrogram
+    plt.subplot(212)
+    spectrogram = plt.specgram(signal, Fs = f, scale_by_freq=True, sides='default')
 
     plt.show()
     spf.close()
