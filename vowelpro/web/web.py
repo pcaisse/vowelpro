3d7e00a9e8fcbf2fc4bc062e4fb0d85e839b891d
Peter Caisse <pcaisse@gmail.com>
2014-11-25 01:12:17 -0500

    Favicon

--- vowelpro/web/web.py
+++ vowelpro/web/web.py
@@ -7,7 +7,7 @@ import json
 
 # Absolute directory path of this file.
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))
-CONFIG_FILE_PATH = 'server.conf'
+GLOBAL_CONFIG_FILE_PATH = 'server.conf'
 
 
 class Speech(object):
@@ -28,9 +28,9 @@ class SpeechWebService(object):
             })
 
 # Global config.
-if os.path.exists(CONFIG_FILE_PATH):
+if os.path.exists(GLOBAL_CONFIG_FILE_PATH):
     # Custom config.
-    cherrypy.config.update(CONFIG_FILE_PATH)
+    cherrypy.config.update(GLOBAL_CONFIG_FILE_PATH)
 else:
     # Default config.
     cherrypy.config.update({
